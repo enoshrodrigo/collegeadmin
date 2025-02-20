26f7c9c01ef058d2f58477c25913f4df1cd118d2
ENOSH RODRIGO <111069060+enoshrodrigo@users.noreply.github.com>
2025-02-21 01:46:46 +0530

    Add favicon, robots.txt, and various asset files; update Vite config and manifest

---
+++ public/build/assets/datepicker-Cunv5XpD.js
@@ -0,0 +1,1 @@
+$(function(){if($("#datePickerExample").length){var e=new Date,t=new Date(e.getFullYear(),e.getMonth(),e.getDate());$("#datePickerExample").datepicker({format:"mm/dd/yyyy",todayHighlight:!0,autoclose:!0}),$("#datePickerExample").datepicker("setDate",t)}});
